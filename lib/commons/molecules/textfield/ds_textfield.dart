5408d647265dc3666ad5ca12fdad75c9acddaefd
joshuaakirafagan <joshua.akirafagan@bfi.co.id>
2021-12-30 15:09:02 +0700

    Change Log : custom text field

---
+++ lib/commons/molecules/textfield/ds_textfield.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class DSCustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final FormFieldValidator<String> validator;
+  final bool obsecure;
+  final bool readOnly;
+  final VoidCallback onTap;
+  final VoidCallback onEditingCompleted;
+  final TextInputType keyboardType;
+  final ValueChanged<String> onChanged;
+  final bool isMulti;
+  final bool autofocus;
+  final bool enabled;
+  final String errorText;
+  final String hintText;
+  final Widget suffix;
+  final Widget prefix;
+  final int maxLength;
+
+  DSCustomTextField(
+      {Key key,
+      this.controller,
+      this.validator,
+      this.keyboardType = TextInputType.text,
+      this.obsecure = false,
+      this.onTap,
+      this.isMulti = false,
+      this.readOnly = false,
+      this.autofocus = false,
+      this.errorText,
+      @required this.hintText,
+      this.suffix,
+      this.prefix,
+      this.enabled = true,
+      this.onEditingCompleted,
+      this.onChanged,
+      this.maxLength})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 4),
+      child: TextFormField(
+          style: const TextStyle(fontSize: 14.0, color: Colors.black),
+          autofocus: autofocus,
+          keyboardType: keyboardType,
+          controller: controller,
+          readOnly: readOnly,
+          maxLength: maxLength ?? 100,
+          decoration: InputDecoration(
+              hintText: hintText,
+              counterText: '',
+              isDense: true,
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Color(0xFFcbcbcb)),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Color(0xFFcbcbcb)),
+              ),
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(color: Color(0xFFcbcbcb)),
+              ),
+              hintStyle: TextStyle(fontSize: 14.0, color: Color(0xFFcbcbcb))),
+          validator: validator),
+    );
+  }
+}
